9745d92efd207721ec5cc1a59d7c5ea50de05430
unknown <evatrancho@gmail.com>
2022-08-06 12:37:38 -0700

    galaxies&stars

---
+++ build/utils/CMakeFiles/jetson-utils.dir/DependInfo.cmake
@@ -0,0 +1,70 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nvidia/jetson-inference/utils/URI.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/URI.cpp.o"
+  "/home/nvidia/jetson-inference/utils/XML.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/XML.cpp.o"
+  "/home/nvidia/jetson-inference/utils/camera/gstCamera.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/camera/gstCamera.cpp.o"
+  "/home/nvidia/jetson-inference/utils/camera/v4l2Camera.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/camera/v4l2Camera.cpp.o"
+  "/home/nvidia/jetson-inference/utils/codec/gstBufferManager.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/codec/gstBufferManager.cpp.o"
+  "/home/nvidia/jetson-inference/utils/codec/gstDecoder.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/codec/gstDecoder.cpp.o"
+  "/home/nvidia/jetson-inference/utils/codec/gstEncoder.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/codec/gstEncoder.cpp.o"
+  "/home/nvidia/jetson-inference/utils/codec/gstUtility.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/codec/gstUtility.cpp.o"
+  "/home/nvidia/jetson-inference/utils/commandLine.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/commandLine.cpp.o"
+  "/home/nvidia/jetson-inference/utils/cuda/cudaBayer.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/cuda/cudaBayer.cpp.o"
+  "/home/nvidia/jetson-inference/utils/cuda/cudaColorspace.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/cuda/cudaColorspace.cpp.o"
+  "/home/nvidia/jetson-inference/utils/cuda/cudaPointCloud.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/cuda/cudaPointCloud.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glBuffer.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glBuffer.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glCamera.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glCamera.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glDisplay.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glDisplay.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glEvents.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glEvents.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glTexture.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glTexture.cpp.o"
+  "/home/nvidia/jetson-inference/utils/display/glWidget.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/display/glWidget.cpp.o"
+  "/home/nvidia/jetson-inference/utils/filesystem.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/filesystem.cpp.o"
+  "/home/nvidia/jetson-inference/utils/image/imageIO.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/image/imageIO.cpp.o"
+  "/home/nvidia/jetson-inference/utils/image/imageLoader.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/image/imageLoader.cpp.o"
+  "/home/nvidia/jetson-inference/utils/image/imageWriter.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/image/imageWriter.cpp.o"
+  "/home/nvidia/jetson-inference/utils/input/devInput.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/input/devInput.cpp.o"
+  "/home/nvidia/jetson-inference/utils/input/devJoystick.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/input/devJoystick.cpp.o"
+  "/home/nvidia/jetson-inference/utils/input/devKeyboard.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/input/devKeyboard.cpp.o"
+  "/home/nvidia/jetson-inference/utils/logging.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/logging.cpp.o"
+  "/home/nvidia/jetson-inference/utils/network/IPv4.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/network/IPv4.cpp.o"
+  "/home/nvidia/jetson-inference/utils/network/NetworkAdapter.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/network/NetworkAdapter.cpp.o"
+  "/home/nvidia/jetson-inference/utils/network/Socket.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/network/Socket.cpp.o"
+  "/home/nvidia/jetson-inference/utils/threads/Process.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/threads/Process.cpp.o"
+  "/home/nvidia/jetson-inference/utils/threads/Thread.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/threads/Thread.cpp.o"
+  "/home/nvidia/jetson-inference/utils/timespec.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/timespec.cpp.o"
+  "/home/nvidia/jetson-inference/utils/video/videoOptions.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/video/videoOptions.cpp.o"
+  "/home/nvidia/jetson-inference/utils/video/videoOutput.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/video/videoOutput.cpp.o"
+  "/home/nvidia/jetson-inference/utils/video/videoSource.cpp" "/home/nvidia/jetson-inference/build/utils/CMakeFiles/jetson-utils.dir/video/videoSource.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ENABLE_NVMM"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "aarch64/include"
+  "aarch64/include/jetson-inference"
+  "aarch64/include/jetson-utils"
+  "/usr/include/gstreamer-1.0"
+  "/usr/lib/aarch64/gstreamer-1.0/include"
+  "/usr/include/glib-2.0"
+  "/usr/include/libxml2"
+  "/usr/lib/aarch64/glib-2.0/include"
+  "/usr/local/cuda/include"
+  "/usr/lib/aarch64-linux-gnu/gstreamer-1.0/include"
+  "/usr/lib/aarch64-linux-gnu/glib-2.0/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
